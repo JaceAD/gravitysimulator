c57d296781a0ed11b80e6dfe8452fe72704f5404
Jace <dembskij0659@my.uwstout.edu>
2018-03-02 15:09:28 -0600

    removed stuff from arrow that was unnecessary. Added a pause value

--- gravitySimulation.py
+++ gravitySimulation.py
@@ -36,15 +36,11 @@ def main():
     clock = pygame.time.Clock()
     
     planets = [];
-    
-    #Strength of arrow shot. Will be divided by 60, adding roughly 1 to force per second
-    powerMeter = 0
-    planetMass = 0
-    originalThrustVector = Vec2d(0,0)
 
     frame_rate = 60
     playback_speed = 4
     dt = playback_speed/frame_rate
+    paused = False
     done = False
     while not done:
         gameMouse = pygame.mouse
@@ -53,56 +49,19 @@ def main():
             if event.type == pygame.QUIT: # If user clicked close
                 done = True
             if event.type == pygame.MOUSEBUTTONDOWN:
-#                if(not arrow.getActive()):
-#                    #Click and hold for power. Release to shoot
-#                    arrow.setCharging(True)
                 print("Will get the vector for the planet to move on")
             if event.type == pygame.MOUSEBUTTONUP:
                 print("Have to send planet in vector from center to mouse")
-#                if(not arrow.getActive()):
-#                    arrow.setActive(True)
-#                    arrow.setCharging(False)
-#                    arrowShootingStart = True
-#                    originalThrustVector = ForceCalculator.calculateThrust(arrow.center, arrow.tip, powerMeter)
-#                    arrow.update_force(originalThrustVector)
-                    
-        if(planet.getActive()):
-            #calculate all forces that would apply to arrow here
-            forces = []
-            for planet in planets:
-                forces.append(ForceCalculator.calculateGravity(planet.center, planet.mass, planet.center, planet.mass, -10))
-            if(arrowShootingTime > 0): #will have to set a boolean that will change to initialize this step
-                forces.append(originalThrustVector)
-            else:
-                #powerMeter = 0 GET the vector from center to mouse.
-            forceSum = ForceCalculator.sumForces(forces)
+        
+        #If the game is not paused, update the objects in it
+        if(not paused):            
             for obj in planets:
-                obj.update_forcew(dt)
-            arrow.update_force(forceSum)
-            print("Arrow is active")
-        else:
-            #Aim input here
-            mouseTup = gameMouse.get_pos()
-            mouseVec = Vec2d(mouseTup[0]-1200, mouseTup[1])
-            coordMouseVec = coords.pos_to_screen(mouseVec).int()
-            if(arrow.getCharging()):
-                powerMeter += 2
-            print("Arrow is deactive")
-
-        print("Power Meter:", powerMeter)
-            # Drawing
+                obj.update_force(dt)
+        
         screen.fill(BLACK) # wipe the screen
         for obj in planets:
             obj.draw(screen, coords) # draw object to screen
             obj.update(dt)
-        
-        #mouseTup = gameMouse.get_pos()
-        #mouseVec = Vec2d(mouseTup[0] - 1200, mouseTup[1])
-        #coordMouseVec = coords.pos_to_screen(mouseVec).int()
-        #arrow.rotateByMouse(coordMouseVec)
-        #mousePosSurface = textFont.render("x: " + str(coordMouseVec[0]) + " y: " + str(coordMouseVec[1]), 0, WHITE)
-        #screen.blit(mousePosSurface, (500,500))
-        #print("Arrow is deactive")
         
         # --- Update the screen with what we've drawn.
         pygame.display.update()
